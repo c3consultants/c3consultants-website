6c291905753c41d728f67e736e8d1b32a9f467a5
Nitin <webskillset@gmail.com>
2025-11-25 20:38:47 +0530

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter, Playfair_Display } from 'next/font/google';
 import './globals.css';
 import Header from '@/app/components/Header';
 import Footer from '@/app/components/Footer';
-import Script from 'next/script';
+import GoogleAnalytics from '@/app/components/GoogleAnalytics';
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -24,29 +24,20 @@ export const metadata = {
   },
   description: 'Best consultancy in Patiala for career guidance & competitive exams. C3 by Manik Raj Singla - 23+ years, 5000+ students, 95% success. SSC, UPSC, Banking coaching.',
   keywords: [
-    // Primary Focus Keywords
     'best consultancy in Patiala',
     'best consultancy Patiala',
     'top consultancy Patiala',
     'career consultancy Patiala',
-    
-    // Service + Location
     'best career counselling Patiala',
     'competitive exam coaching Patiala',
     'SSC coaching Patiala',
     'UPSC coaching Patiala',
     'banking coaching Patiala',
-    
-    // Brand
     'C3 Career Consultants',
     'Manik Raj Singla',
-    
-    // Long-tail
     'best career consultancy Patiala Punjab',
     'top coaching institute Patiala',
     'government exam coaching Patiala',
-    
-    // Specific
     'SSC CGL coaching Patiala',
     'IBPS coaching Patiala',
     'career guidance Patiala',
@@ -132,52 +123,8 @@ export default function RootLayout({ children }) {
         <link rel="preload" as="image" href="/images/c3-logo.jpg" />
       </head>
       <body className="font-sans antialiased bg-white text-neutral-800">
-        {/* Google Analytics - GA4 */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-60D0V6YTH2"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-60D0V6YTH2');
-            `,
-          }}
-        />
-
-        {/* Facebook Pixel */}
-        <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', 'YOUR_PIXEL_ID_HERE');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID_HERE&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
+        {/* Google Analytics Component */}
+        <GoogleAnalytics />
 
         <Header />
         <main className="min-h-screen">
